1e4991f5ec25d0876b1297d09194ef679fdce998
Leanne Werner <leanne.werner.lw@gmail.com>
2023-07-26 23:45:50 -0700

    use mask

--- packages/example/pages/experience.tsx
+++ packages/example/pages/experience.tsx
@@ -1,4 +1,4 @@
-import { Environment, MeshPortalMaterial, OrbitControls, Svg } from "@react-three/drei"
+import { Environment, Mask, ScrollControls, useMask, useScroll } from "@react-three/drei"
 import { Canvas, useFrame, useLoader } from "@react-three/fiber"
 import { useRef } from "react"
 import * as THREE from "three"
@@ -6,13 +6,30 @@ import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader"
 import "../app/canvas.css"
 
 function Logo() {
-  const portal = useRef<any>()
+  const mesh = useRef<any>()
+  const maskRef = useRef<any>()
+  const ringRef = useRef<any>()
   const shapes = useLoader(SVGLoader, "/reforest.svg")
+  const scroll = useScroll()
+  const stencil = useMask(1, false)
+
+  useFrame((state, delta) => {
+    const offset = scroll.offset
+    const portalScale = 1.0 + offset * 2 // Scale the portal down
+    maskRef.current.scale.set(portalScale, portalScale, portalScale)
+    ringRef.current.scale.set(portalScale, portalScale, portalScale)
+  })
 
   return (
-    <mesh>
-      <circleGeometry args={[50, 100]} />
-      <MeshPortalMaterial ref={portal} side={THREE.DoubleSide}>
+    <>
+      <mesh ref={ringRef}>
+        <ringGeometry args={[50, 51, 100]} />
+        <meshStandardMaterial color="#B8860B" metalness={1.0} roughness={0.1} />
+      </mesh>
+      <Mask ref={maskRef} id={1} colorWrite depthWrite={false}>
+        <circleGeometry args={[50, 100]} />
+      </Mask>
+      <mesh ref={mesh}>
         <group scale={[0.1, 0.1, 0.1]} rotation={[Math.PI, 0, 0]} position={[-120, 100, -10]}>
           {shapes.paths.map((path, index) => (
             <mesh key={index}>
@@ -21,27 +38,21 @@ function Logo() {
                 attach="material"
                 color={new THREE.Color().setHSL(index / shapes.paths.length, 1, 0.5)}
                 side={THREE.DoubleSide}
+                {...stencil}
               />
             </mesh>
           ))}
         </group>
-      </MeshPortalMaterial>
-    </mesh>
+      </mesh>
+    </>
   )
 }
 
 function Scene() {
-  const svgRef = useRef<any>()
-  const shapes = useLoader(SVGLoader, "/reforest.svg")
-
   return (
-    <>
-      <mesh>
-        <ringGeometry args={[50, 51, 100]} />
-        <meshStandardMaterial color="#B8860B" metalness={1.0} roughness={0.1} />
-      </mesh>
+    <ScrollControls>
       <Logo />
-    </>
+    </ScrollControls>
   )
 }
 
@@ -67,7 +78,6 @@ export default function App() {
         <Environment preset="sunset" />
         <pointLight position={[0, 0, 10]} color="deeppink" intensity={10} />
         <Scene />
-        <OrbitControls />
       </Canvas>
     </div>
   )
