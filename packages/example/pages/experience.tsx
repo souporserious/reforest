98c8cb0c1c5b0d7869a427404bbfeab5f3f7a875
Leanne Werner <leanne.werner.lw@gmail.com>
2023-07-27 22:31:48 -0700

    adding index to paths

--- packages/example/pages/experience.tsx
+++ packages/example/pages/experience.tsx
@@ -1,23 +1,58 @@
-import { Environment, Mask, ScrollControls, useMask, useScroll } from "@react-three/drei"
+import {
+  Environment,
+  Mask,
+  OrbitControls,
+  ScrollControls,
+  useMask,
+  useScroll,
+} from "@react-three/drei"
 import { Canvas, useFrame, useLoader } from "@react-three/fiber"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import * as THREE from "three"
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader"
 import "../app/canvas.css"
 
+function CustomShape({ path, index }: { path: any; index: number }) {
+  const stencil = useMask(1, false)
+  const shapeRef = useRef<any>()
+  const scroll = useScroll()
+
+  useFrame((state, delta) => {
+    const offset = scroll.offset
+
+    // move the shape in the negative x direction based on the scroll offset and the index
+    shapeRef.current.position.z = -offset * 2 - index * 15
+  })
+
+  return (
+    <mesh ref={shapeRef}>
+      <shapeGeometry attach="geometry" args={[path.toShapes(true), 50]} />
+      <meshBasicMaterial
+        attach="material"
+        color={path.color}
+        side={THREE.DoubleSide}
+        {...stencil}
+      />
+    </mesh>
+  )
+}
 function Logo() {
   const mesh = useRef<any>()
   const maskRef = useRef<any>()
   const ringRef = useRef<any>()
+
   const shapes = useLoader(SVGLoader, "/reforest.svg")
   const scroll = useScroll()
-  const stencil = useMask(1, false)
 
   useFrame((state, delta) => {
     const offset = scroll.offset
     const portalScale = 1.0 + offset * 2 // Scale the portal down
     maskRef.current.scale.set(portalScale, portalScale, portalScale)
     ringRef.current.scale.set(portalScale, portalScale, portalScale)
+
+    // move the camera to look at the side of the logo
+    state.camera.position.x = offset * 30
+    state.camera.lookAt(mesh.current.position)
   })
 
   return (
@@ -28,19 +63,12 @@ function Logo() {
       </mesh>
       <Mask ref={maskRef} id={1} colorWrite depthWrite={false}>
         <circleGeometry args={[50, 100]} />
+        <meshBasicMaterial color="black" />
       </Mask>
       <mesh ref={mesh}>
-        <group scale={[0.1, 0.1, 0.1]} rotation={[Math.PI, 0, 0]} position={[-120, 100, -10]}>
+        <group scale={[0.1, 0.1, 0.1]} rotation={[Math.PI, 0, 0]} position={[-120, 100, -100]}>
           {shapes.paths.map((path, index) => (
-            <mesh key={index}>
-              <shapeGeometry attach="geometry" args={[path.toShapes(true)]} />
-              <meshBasicMaterial
-                attach="material"
-                color={new THREE.Color().setHSL(index / shapes.paths.length, 1, 0.5)}
-                side={THREE.DoubleSide}
-                {...stencil}
-              />
-            </mesh>
+            <CustomShape key={index} path={path} index={index} />
           ))}
         </group>
       </mesh>
@@ -76,8 +104,8 @@ export default function App() {
         <color attach="background" args={["#171d6c"]} />
         <ambientLight />
         <Environment preset="sunset" />
-        <pointLight position={[0, 0, 10]} color="deeppink" intensity={10} />
         <Scene />
+        {/* <OrbitControls /> */}
       </Canvas>
     </div>
   )
